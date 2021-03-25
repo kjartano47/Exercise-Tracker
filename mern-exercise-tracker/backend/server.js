c47459421ce8979255ecb0efc691c28f92d3f1c4
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2021-03-25 11:26:20 +0000

    server is now able to connect to MongoDB

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -1,7 +1,7 @@
-//
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+
 //Configures so that environment variables can be set it the dotenv file
 require("dotenv").config();
 
@@ -12,6 +12,19 @@ const port = process.env.PORT || 5000;
 //Cors middleware and allows us to parse json
 app.use(cors());
 app.use(express.json());
+
+//Connection to MongoDB Atlas
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+});
+//Once connection is open, a message is logged
+const connection = mongoose.connection;
+connection.once("open", () => {
+	console.log("MongoDB database connection established successfully");
+});
 
 //Here we start the server and listen to the port given above
 app.listen(port, () => {
